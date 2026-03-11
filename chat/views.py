4388b038c01485671700fb48a621d88d501268c2
Paco Woo <woopaco@gmail.com>
2026-03-11 13:21:39 -0600

    Append last history message to messages list

--- chat/views.py
+++ chat/views.py
@@ -620,7 +620,7 @@ CEA: control y elementos de Automatizacion
         else:
         #Se añade el historial de mensajes de la app front al de la API
             messages = self.messages.copy()
-            
+            messages.append(history[-1])
             try:
                 #Se llama al metodo chat y se pasa como parametro el historial de mensajes Global
                 response = self.chat(messages)
