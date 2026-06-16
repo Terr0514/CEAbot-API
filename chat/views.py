0984381b961371c0646c6982b4ff3450e680d21c
Terr0514 <woopaco@gmail.com>
2026-06-16 05:09:28 -0600

    Locations Changed and Update

--- chat/views.py
+++ chat/views.py
@@ -43,7 +43,7 @@ class CeaBot_API(APIView):
         self.apiKey4 = settings.API_KEY4
         #PARAMETROS OPENROUTER
         self.url = "https://openrouter.ai/api/v1"
-        self.model = "openrouter/owl-alpha"
+        self.model = "openrouter/fusion"
         self.messages = []
         self.mail = EmailMessage()
         #CREDENCIALES EMAIL
